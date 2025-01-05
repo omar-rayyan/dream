f0ccce7a2ef403b9afb2e4f29a3d41e0af2da584
omar-rayyan <omarrayyan999@gmail.com>
2025-01-05 13:29:30 +0200

    Modified the deployment war file.

--- src/main/java/com/axsosacademy/dream/DreamApplication.java
+++ src/main/java/com/axsosacademy/dream/DreamApplication.java
@@ -2,12 +2,28 @@ package com.axsosacademy.dream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class DreamApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DreamApplication.class, args);
 	}
+	
+	@Bean
+    public TomcatServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        Connector ajpConnector = new Connector("AJP/1.3");
+        ajpConnector.setPort(8081);
+        ajpConnector.setSecure(false);
+        ajpConnector.setAllowTrace(false);
+        ajpConnector.setScheme("http");
+        ((AbstractAjpProtocol<?>)ajpConnector.getProtocolHandler()).setSecretRequired(false);
+        tomcat.addAdditionalTomcatConnectors(ajpConnector);
+        return tomcat;
+    }
 
 }
